377375a6d3842be1eb74fb5a6787803adc2fde88
uzapolsky <uzapolsky@gmail.com>
2021-09-13 08:29:06 +0300

    add curses and one shining star

---
+++ space_trash_game.py
@@ -0,0 +1,25 @@
+import time
+import curses
+
+
+def draw(canvas):
+    row, column = (5, 20)
+    canvas.border()
+    curses.curs_set(False)
+    while True:
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+  
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
