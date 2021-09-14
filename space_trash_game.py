33b6146fdc550229b0d7dd8e106fccf78aacf4ea
uzapolsky <uzapolsky@gmail.com>
2021-09-14 11:37:49 +0300

    add furst coroutine

--- space_trash_game.py
+++ space_trash_game.py
@@ -1,24 +1,31 @@
-import time
+import asyncio
 import curses
+import time
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
-    row, column = (5, 20)
-    canvas.border()
-    curses.curs_set(False)
-    while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
+
+    coroutine = blink(canvas, 5, 5)
+    for i in range (4):
+        (coroutine.send(None))
+        canvas.refresh() 
         time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+    time.sleep(3)
   
 if __name__ == '__main__':
     curses.update_lines_cols()
